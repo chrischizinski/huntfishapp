19715942341dad689f4a4b711565b56ce0d3a755
Christopher Chizinski <chris.chizinski@gmail.com>
2020-08-25 15:59:00 -0500

    add footer

--- inst/shiny_app/ui.R
+++ inst/shiny_app/ui.R
@@ -10,7 +10,17 @@ fluidPage(
       rel = "stylesheet",
       type = "text/css",
       href = "style.css"
-    )))
+    ))),
+    #end dashboardPage
+    tags$footer("My footer", align = "center", style = "
+              position:absolute;
+              bottom:0;
+              width:100%;
+              height:50px;   /* Height of the footer */
+              color: white;
+              padding: 10px;
+              background-color: black;
+              z-index: 1000;")
   ),
 
   # Main UI -------------------------------------------------------------
