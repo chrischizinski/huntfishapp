e46e3f5753f694608531aaf2f553bfa47cac1636
Christopher Chizinski <chris.chizinski@gmail.com>
2020-08-26 11:11:39 -0500

    add email to

--- inst/shiny_app/main.R
+++ inst/shiny_app/main.R
@@ -124,7 +124,8 @@ mainUI <- function(id) {
                                        further analysis and review. No warranty, expressed or implied, is made by the USGS or the U.S. Government as to
                                        the functionality of the software and related material nor shall the fact of release constitute any such warranty.
                                        Furthermore, the software is released on condition that neither the USGS nor the U.S. Government shall be held
-                                       liable for any damages resulting from its authorized or unauthorized use.")
+                                       liable for any damages resulting from its authorized or unauthorized use."),
+                 tags$a(href="mailto:cchizinski2@unl.edu", "Email: Chris Chizinski")
                  )
         ,
 
